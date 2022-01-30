966b748e11806e2616e520b27cf06b5cb282531f
VishalVinayRam <89966360+VishalVinayRam@users.noreply.github.com>
2022-01-30 12:32:23 +0530

    Update NoteViewModel.kt

--- app/src/main/java/com/languagexx/simplenotes/ui/NoteViewModel.kt
+++ app/src/main/java/com/languagexx/simplenotes/ui/NoteViewModel.kt
@@ -12,31 +12,25 @@ class NoteViewModel @Inject constructor(
 ) : ViewModel() {
 
 
-    //Database Operations in view model
 
 
-    // Method #1
     fun insert(note: Note) {
         return noteRepository.insert(note)
     }
 
-    // Method #2
     fun delete(note: Note) {
         noteRepository.delete(note)
     }
 
-    // Method #3
     fun deleteById(id:Int){
         noteRepository.deleteById(id)
     }
 
-    // Method #4
     fun update(note: Note) {
         Log.e("DEBUG","update is called in viewmodel")
         noteRepository.update(note)
     }
 
-    // Method #5
     fun getAllNotes(): LiveData<List<Note>> {
         Log.e("DEBUG", "View model getallnotes")
         return noteRepository.getAllNotes()
